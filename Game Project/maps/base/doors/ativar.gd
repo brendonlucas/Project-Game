3030dc491fab146476d4b0d5f2747f50f21015c0
brendonlucas <brendonplay007@gmail.com>
2020-10-10 20:20:46 -0300

    notas

--- Game Project/maps/base/doors/ativar.gd
+++ Game Project/maps/base/doors/ativar.gd
@@ -13,10 +13,11 @@ func interact():
 	if open and get_parent().get_node(".").get_active():
 		dor.play("close")
 		open = false
-			
+		get_parent().get_node("dor").play()
 	elif !open and get_parent().get_node(".").get_active():
 		dor.play("open")
 		open = true
+		get_parent().get_node("dor").play()
 	elif !get_parent().get_node(".").get_active():
 		get_tree().get_root().get_node("Map/Controler_map").start_legenda_elevador()
 func set_open(option):
--- Game Project/props/Scifi_wall/parts/dor.gd
+++ Game Project/props/Scifi_wall/parts/dor.gd
@@ -9,10 +9,11 @@ func _on_Area_open_body_entered(body):
 	pass
 	if body.name == "Player_v4":
 		$AnimationPlayer.play("open")
-	
+		$door_s.play()
 
 
 func _on_Area_open_body_exited(body):
 	pass
 	if body.name == "Player_v4":
 		$AnimationPlayer.play("close")
+		$door_s.play()
